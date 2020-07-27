17e8eed353b697638af275b78ae5905267205841
Ahmed Bajramovic <ahmed.kemal.bajramovic@gmail.com>
2020-07-28 01:37:31 +0200

    move javascript function to separate file

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+myFunction();
